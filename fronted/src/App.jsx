877cb035f5035ab1144147c7316ad5912415e7bb
Sudip Mukherjee <sudipmukherjee96144@gmail.com>
2024-04-19 13:08:50 +0530

    resume bug fixed

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,45 +1,61 @@
-import React, { useRef, useState, useCallback, createRef } from 'react';
-import './App.css';
+import React, { useRef, useState, useCallback, createRef } from "react";
+import "./App.css";
 import Webcam from "react-webcam";
-import axios from 'axios';
-import { Header, Grid, Button, Icon, Message, Loader } from 'semantic-ui-react';
+import axios from "axios";
+import { Header, Grid, Button, Icon, Message, Loader } from "semantic-ui-react";
 
 function App() {
   const webcamRef = useRef(null);
+  const utteranceRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
   const [textOcr, setTextOcr] = useState(null);
   const [load, setLoad] = useState(false);
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+  const [speechInProgress, setSpeechInProgress] = useState(false);
   let fileInputRef = createRef();
 
   // Function to handle speech synthesis
   const speak = (text) => {
     const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = 'en-US';
+    (utterance.lang = "en-US");
+    utteranceRef.current = utterance;
     speechSynthesis.cancel();
     speechSynthesis.speak(utterance);
+    setButtonDisabled(false);
   };
 
   // Function to handle pausing speech synthesis
   const pauseSpeech = () => {
     speechSynthesis.pause();
+    
   };
 
   // Function to handle resuming speech synthesis
   const resumeSpeech = () => {
-    speechSynthesis.resume();
+    if (utteranceRef.current) {
+      speechSynthesis.resume();
+      
+      setSpeechInProgress(true); // Set speech in progress
+    } else {
+      alert("Please Speak!");
+      setButtonDisabled(true);
+      
+// Disable the button
+    }
   };
 
   const capture = useCallback(() => {
     setLoad(true);
     const imageSrc = webcamRef.current.getScreenshot();
-    let url = 'http://localhost:5000/capture';
+    let url = "http://localhost:5000/capture";
     let config = {
-      headers: {'Content-Type': 'application/json'}
+      headers: { "Content-Type": "application/json" },
     };
     let dataBody = {
-      img: imageSrc
+      img: imageSrc,
     };
-    axios.post(url, dataBody, config)
+    axios
+      .post(url, dataBody, config)
       .then((res) => {
         setTextOcr(res.data.text);
         setImgSrc(imageSrc);
@@ -50,26 +66,34 @@ function App() {
       });
   }, [webcamRef, setImgSrc]);
 
-  const upload = (file) => {
+  const upload = async (file) => {
     setLoad(true);
-    var url = 'http://localhost:5000/upload';
+    var url = "http://localhost:5000/upload";
     var formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
     var config = {
-      headers: {'Content-Type': 'multipart/form-data'}
+      headers: { "Content-Type": "multipart/form-data" },
     };
-    return axios.post(url, formData, config)
-      .then((res) => {
-        setTextOcr(res.data.text);
-        setImgSrc(res.data.image);
-        setLoad(false);
-      });
+    await axios.post(url, formData, config).then((res) => {
+      setTextOcr(res.data.text);
+      setImgSrc(res.data.image);
+      setLoad(false);
+      utteranceRef.current = null;
+    });
+  };
+
+  const handleOnChangeFile = (e) => {
+    const file = e.target.files[0];
+    upload(file);
   };
 
   return (
     <>
       <center>
-        <Header style={{ margin: 40, fontSize: 50, fontFamily: 'roboto' }} size='huge'>
+        <Header
+          style={{ margin: 40, fontSize: 50, fontFamily: "roboto" }}
+          size="huge"
+        >
           React OCR
         </Header>
       </center>
@@ -83,19 +107,37 @@ function App() {
               screenshotFormat="image/jpeg"
             />
             <Grid.Column>
-              <Button size='big' onClick={capture}
-                style={{ margin: 20 }} icon labelPosition='left' inverted color='green'>
-                <Icon name='camera' />
+              <Button
+                size="big"
+                onClick={capture}
+                style={{ margin: 20 }}
+                icon
+                labelPosition="left"
+                inverted
+                color="green"
+              >
+                <Icon name="camera" />
                 Capture
               </Button>
 
-              <Button size='big' onClick={() => fileInputRef.current.click()}
-                style={{ margin: 20 }} icon labelPosition='left' inverted color='blue'>
-                <Icon name='upload' />
+              <Button
+                size="big"
+                onClick={() => fileInputRef.current.click()}
+                style={{ margin: 20 }}
+                icon
+                labelPosition="left"
+                inverted
+                color="blue"
+              >
+                <Icon name="upload" />
                 Upload
                 <form encType="multipart/form-data">
-                  <input ref={fileInputRef} type='file' hidden name='filename'
-                    onChange={(x) => { upload(x.target.files[0]) }}
+                  <input
+                    ref={fileInputRef}
+                    type="file"
+                    hidden
+                    name="filename"
+                    onChange={handleOnChangeFile}
                     accept="image/*"
                   />
                 </form>
@@ -105,40 +147,45 @@ function App() {
               <div style={{ marginTop: 20 }}>
                 <Button onClick={() => speak(textOcr)}>Play</Button>
                 <Button onClick={pauseSpeech}>Pause</Button>
-                <Button onClick={resumeSpeech}>Resume</Button>
+                <Button onClick={resumeSpeech} disabled={isButtonDisabled}>Resume</Button>
               </div>
             </Grid.Column>
           </center>
         </Grid.Column>
 
         <Grid.Column style={{ width: "50%" }} key={1}>
-          {
-            load
-              ?
-              <Loader style={{ marginTop: 120 }} active inline='centered' size='big'>Loading...</Loader>
-              :
-              (
-                imgSrc
-                  ?
-                  <>
-                    <Header style={{ margin: 10, fontFamily: 'roboto' }} size='large'>
-                      Result
-                    </Header>
-                    <img style={{ marginLeft: 10, height: '50%' }} alt="captured" src={imgSrc} />
-                    <Message
-                      size='massive'
-                      color='orange'
-                      header={textOcr}
-                      content=""
-                      style={{ margin: 15 }}
-                    />
-                  </>
-                  :
-                  <Header style={{ margin: 10, fontFamily: 'roboto' }} size='large'>
-                    No data preview
-                  </Header>
-              )
-          }
+          {load ? (
+            <Loader
+              style={{ marginTop: 120 }}
+              active
+              inline="centered"
+              size="big"
+            >
+              Loading...
+            </Loader>
+          ) : imgSrc ? (
+            <>
+              <Header style={{ margin: 10, fontFamily: "roboto" }} size="large">
+                Result
+              </Header>
+              <img
+                style={{ marginLeft: 10, height: "50%" }}
+                alt="captured"
+                src={imgSrc}
+              />
+              <Message
+                size="massive"
+                color="orange"
+                header={textOcr}
+                content=""
+                style={{ margin: 15 }}
+              />
+            </>
+          ) : (
+            <Header style={{ margin: 10, fontFamily: "roboto" }} size="large">
+              No data preview
+            </Header>
+          )}
         </Grid.Column>
       </Grid>
     </>
